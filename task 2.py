299b70f92a10e8a622ed4595709a5a2e6fc4f3eb
24SBCS100 <24sbcs100@psgrkcw.ac.in>
2026-01-02 13:25:25 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,30 @@
+class BankAccount:
+    def __init__(self, account_holder, balance=0):
+        self.account_holder = account_holder
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited: {amount}")
+            print(f"Current Balance: {self.balance}")
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            print("Withdrawal amount must be positive.")
+        elif amount > self.balance:
+            print("Insufficient balance.")
+        else:
+            self.balance -= amount
+            print(f"Withdrawn: {amount}")
+            print(f"Current Balance: {self.balance}")
+
+
+
+account = BankAccount("Alex", 1000)
+
+account.deposit(500)
+account.withdraw(300)
+account.withdraw(1500)
